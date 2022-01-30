1fc66d55133860d5f53792a9c9e28b9dc0ef5298
EduardDH <eduard.zaynullin@deliveryhero.com>
2022-01-30 22:39:17 +0100

    Add sqlite

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type E struct {
-	Events string
+// The model.
+type User struct {
+	gorm.Model
+	Name string `json:"name"`
 }
 
 func main() {
+	// Open a new connection to our sqlite database.
+	db, err := gorm.Open("sqlite3", "database.db")
+
+	if err != nil {
+		panic("Failed to open the SQLite database.")
+	}
+	// Create the table from our struct.
+	db.AutoMigrate(&User{})
+
+	// Create a new user in our database.
+	db.Create(&User{
+		Name: "Craig",
+	})
+
+	// Find all of our users.
+	var users []User
+	db.Find(&users)
+
+	// Output the users from the DB json encoded
+	jsonEncoded, _ := json.Marshal(&users)
+	fmt.Println(string(jsonEncoded))
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.tmpl")
 
@@ -26,6 +54,7 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": string(x),
 		})
+
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
