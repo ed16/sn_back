abe543aca9209f42542436dd621a55744a2019da
EduardDH <eduard.zaynullin@deliveryhero.com>
2022-01-30 12:20:12 +0100

    Process POST request

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"github.com/gin-gonic/gin"
 )
+
+type E struct {
+	Events string
+}
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	// r.GET("/sendMessage", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	r.POST("/sendMessage", func(c *gin.Context) {
+		x, _ := ioutil.ReadAll(c.Request.Body)
+
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": string(x),
 		})
 	})
 
